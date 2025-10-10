657a376069931d8d53709a22a2ada8677a4b00fb
swathy1993 <swathy.t.m@ibm.com>
2025-10-10 14:55:32 +0530

    Add files via upload

---
+++ Cucumber/Activities/DataTable.java
@@ -0,0 +1,42 @@
+package stepDefinition;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DataTable extends BaseClass {
+
+	@Given("user is on the To-Do list page")
+	public void openPage() {
+		driver.get("https://training-support.net/webelements/todo-list");
+		assertEquals(driver.getTitle(), "Selenium: To-Do List");
+	}
+
+	@When("user adds the following tasks")
+	public void inputTasks(DataTable inputTasks) throws InterruptedException {
+		List<String> tasks = inputTasks.asList();
+		System.out.println(tasks);
+
+		for(String task : tasks) {
+			driver.findElement(By.id("todo-input")).sendKeys(task);
+			driver.findElement(By.id("todo-add")).click();
+			Thread.sleep(2000);
+		}
+	}
+
+	private List<String> asList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Then("they can see the task added to the list")
+	public void verifyResults() {
+		System.out.println("All tasks present");
+	}
+}
